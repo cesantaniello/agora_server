0a9d5b081e74ae6a962236986974ad7cacc2c310
Administrator <admin@example.com>
2022-02-24 16:57:00 +0100

    Crea ficheros iniciales de servidor

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '97d79da16711f4a97f12450836d25428'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fbcb8b36-a7e5-4f7b-bce2-78f690667feb'
+};
